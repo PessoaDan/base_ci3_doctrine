0271811c1bd6910da9766bb1b859918fa4b7bd8d
Daniel Pessoa <dabiel@lti.net.br>
2021-03-25 10:05:44 -0300

    initial_commit

---
+++ application/models/entidades/Entidade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace models\entidades;
+
+
+/**
+ * @class Entidade
+ * @Entity
+ */
+class Entidade {
+
+    /**
+     * @var int $id
+     * @Id @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+
+}
